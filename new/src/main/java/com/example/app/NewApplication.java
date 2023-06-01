ba6358fc85f578f175cc67abc6613d247ec072b3
sayli vaidya <sayli_vaidya@persistent.com>
2023-06-01 13:18:18 +0530

    Resolved

--- new/src/main/java/com/example/app/NewApplication.java
+++ new/src/main/java/com/example/app/NewApplication.java
@@ -2,7 +2,7 @@ package com.example.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//application
 @SpringBootApplication
 public class NewApplication {
 
--- new/src/main/java/com/example/app/repo/EmployeeRepo.java
+++ new/src/main/java/com/example/app/repo/EmployeeRepo.java
@@ -2,7 +2,7 @@ package com.example.app.repo;
 
 import com.example.app.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+//this is emp repo
 public interface EmployeeRepo extends JpaRepository<Employee ,Integer> {
 
 
